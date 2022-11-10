277ef42e0e97c36e30ef57698ba6d67685a00393
Cupcak3 <velozalec97@gmail.com>
2022-11-09 20:10:57 -0800

    Fixed settings provider being undone.

--- InventoryKamera/Properties/Settings.Designer.cs
+++ InventoryKamera/Properties/Settings.Designer.cs
@@ -7,12 +7,14 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.Configuration;
 
 namespace InventoryKamera.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [SettingsProvider(typeof(JsonUserSettingsProvider))]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
