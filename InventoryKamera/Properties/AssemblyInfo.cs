d83224b0bddbac0fe9525085fc334eedcd1b40aa
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-13 22:21:08 +0530

    Remove extra space

--- InventoryKamera/Properties/AssemblyInfo.cs
+++ InventoryKamera/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrew De La Fuente")]
 [assembly: AssemblyProduct("InventoryKamera")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
