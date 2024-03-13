7a78cf335d40cf07245c45e289927fcbd586b5c4
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-03-13 22:21:20 +0530

    Bump version

--- InventoryKamera/Properties/AssemblyInfo.cs
+++ InventoryKamera/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.14.*")]
+[assembly: AssemblyVersion("1.3.15.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
